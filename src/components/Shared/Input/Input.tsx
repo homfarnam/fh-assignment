7c9f44748db8354bb14c0fb522031858be3210b7
Farnam Homayounfard <farnam@getopensocial.com>
2022-08-02 23:30:31 +0200

    Create input component

---
+++ src/components/Shared/Input/Input.tsx
@@ -0,0 +1,39 @@
+import React from "react"
+
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  type?: string
+  placeholder?: string
+  className?: React.HTMLAttributes<HTMLInputElement>["className"]
+
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+  value?: string
+  firstIcon?: React.ReactNode
+  secondIcon?: React.ReactNode
+}
+
+const Input: React.FC<InputProps> = ({
+  type = "text",
+  placeholder = "",
+  className = "",
+  onChange,
+  value = "",
+  firstIcon,
+  secondIcon,
+  ...props
+}) => {
+  return (
+    <>
+      {firstIcon}
+      <input
+        type={type}
+        placeholder={placeholder}
+        className={className}
+        onChange={(e) => onChange(e)}
+        {...props}
+      />
+      {secondIcon}
+    </>
+  )
+}
+
+export default Input

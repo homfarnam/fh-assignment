c9503602d14d5dd6d92b69d723a27856114f9027
Farnam Homayounfard <farnam@getopensocial.com>
2022-08-02 23:20:54 +0200

    Add react index file

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,7 @@
-import ReactDOM from 'react-dom/client'
-import "./styles/tailwind.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
-import React from 'react';
+import ReactDOM from "react-dom/client"
+import "./styles/tailwind.css"
+import App from "./App"
+import React from "react"
+import "react-datepicker/dist/react-datepicker.css"
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />)

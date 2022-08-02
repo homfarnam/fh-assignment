d39dfe1c1b59f34cf04e0083a3bcf8dfc5d5a8a4
Farnam Homayounfard <farnam@getopensocial.com>
2022-08-03 00:11:19 +0200

    Use react context provider
    - Wrap the app with hotel provider

--- src/index.tsx
+++ src/index.tsx
@@ -3,5 +3,10 @@ import "./styles/tailwind.css"
 import App from "./App"
 import React from "react"
 import "react-datepicker/dist/react-datepicker.css"
+import HotelProvider from "context/Provider"
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <HotelProvider>
+    <App />
+  </HotelProvider>
+)

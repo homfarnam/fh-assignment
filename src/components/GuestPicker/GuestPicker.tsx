92e7918a59cf30e9e05253d3456aa3a3ebc748ab
Farnam Homayounfard <farnam@getopensocial.com>
2022-08-03 23:26:21 +0200

    Creata serializer function to send data to the url

--- src/components/GuestPicker/GuestPicker.tsx
+++ src/components/GuestPicker/GuestPicker.tsx
@@ -5,28 +5,40 @@ import CreateRoom from "components/CreateRoom/CreateRoom"
 import { Button } from "components"
 import { SearchButton } from "components/SearchHotel/SearchHotel.styles"
 import { GuestPickerContainer } from "./GuestPicker.styles"
+import { CloseButtonType } from "types/types"
+import { calcTotal } from "lib/lib"
 
 interface GuestPickerProps {
-  handleOpen: () => void
+  handleOpen: (type: CloseButtonType) => void
 }
 
 const GuestPicker: React.FC<GuestPickerProps> = ({ handleOpen }) => {
   const { rooms } = useContext(HotelContext)
 
-  // sum of adults in all rooms
-  let adults = rooms.reduce((acc, curr) => {
-    return acc + curr.adults
-  }, 0)
+  // function to serliaze the data to be sent to the url
+  const serializeData = () => {
+    const serializedData = rooms
+      .map((room) => {
+        const adults = room.adults.toString()
+        const children = room?.children
+          ? room?.children?.map((child) => child.age.toString()).join(",")
+          : ""
+        return adults && children ? `${adults}:${children}` : `${adults}`
+      })
+      .join("|")
 
-  // sum of children in all rooms
-  let children = rooms.reduce((acc, curr) => {
-    return curr?.children ? acc + +curr?.children?.length : 0
-  }, 0)
+    //  add serializedData to current url
+    const url = new URL(window.location.href)
+    url.searchParams.set("rooms", encodeURIComponent(serializedData))
+    window.history.pushState({}, "", url.href)
+  }
+
+  const sumAll = calcTotal(rooms)
 
   return (
     <GuestPickerContainer>
       <div className="flex items-center justify-start w-full py-5 px-3">
-        <Close className="cursor-pointer" onClick={handleOpen} />
+        <Close className="cursor-pointer" onClick={() => handleOpen("Close")} />
         <h3 className="justify-self-center mx-auto text-black">
           Who is staying?
         </h3>
@@ -38,8 +50,13 @@ const GuestPicker: React.FC<GuestPickerProps> = ({ handleOpen }) => {
         })}
       </div>
       <div>
-        <Button type="button" variant="primary" className={SearchButton}>
-          Search - {rooms.length} Rooms - {adults + children} Guests
+        <Button
+          type="button"
+          variant="primary"
+          className={SearchButton}
+          onClick={() => serializeData()}
+        >
+          Search - {rooms.length} Rooms - {sumAll} Guests
         </Button>
       </div>
     </GuestPickerContainer>

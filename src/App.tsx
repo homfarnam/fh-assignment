0a236db9600a47b8580e09cfe6faa663630cd3d8
Farnam Homayounfard <farnam@getopensocial.com>
2022-08-03 23:34:55 +0200

    Create function to decode url param

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,58 @@
+import { useContext, useEffect } from "react"
+import { v4 as uuidv4 } from "uuid"
 import { ReactComponent as Logo } from "./assets/top-logo.svg"
 import mainscene from "./assets/main-scene.png"
 import brands from "./assets/brands.png"
 import { SearchHotel } from "components"
 import { MainDiv } from "styles/main.styles"
+import { Room } from "types/types"
+import { HotelContext } from "context/Provider"
 
 const App = () => {
+  const { setRooms, setBrowserUrl, browserUrl } = useContext(HotelContext)
+
+  useEffect(() => {
+    //  get url and decode rooms parameter
+    const url = window.location.href
+    const urlParams = new URL(url)
+    const rooms = urlParams.searchParams.get("rooms") as string
+
+    setBrowserUrl(decodeURIComponent(rooms))
+  }, [setBrowserUrl])
+
+  // create function to decode rooms parameter and create rooms with Room structure and children with Child structure
+  const decodeRooms = (rooms: string) => {
+    const roomsArray = rooms?.split("|")
+    const roomsDecoded = roomsArray?.map((room, i) => {
+      const roomArray = room.split(":")
+
+      const adults = +roomArray?.[0]
+
+      const children = roomArray?.[1]
+
+      const childrenArray = children?.split(",")
+
+      const childrenDecoded = childrenArray?.map((child) => ({
+        id: uuidv4(),
+        age: parseInt(child),
+      }))
+
+      return {
+        id: uuidv4(),
+        adults,
+        children: childrenDecoded,
+      }
+    })
+    console.log({ roomsDecoded })
+    return roomsDecoded
+  }
+
+  useEffect(() => {
+    console.log({ browserUrl })
+    const allRooms = decodeRooms(browserUrl)
+    setRooms(allRooms as Room[])
+  }, [setRooms, browserUrl])
+
   return (
     <div className="flex flex-col items-center justify-center h-screen text-red-300 bg-gradient-to-br from-gray-300 via-teal-700 to-gray-800">
       <MainDiv

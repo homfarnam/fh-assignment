519e5edad8594f4b8c20aa9360158cede0c04a5e
Farnam Homayounfard <farnam@getopensocial.com>
2022-08-03 19:17:55 +0200

    Import new components

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,4 +1,8 @@
 import SearchHotel from "./SearchHotel/SearchHotel"
 import Input from "./Shared/Input/Input"
+import GuestPicker from "./GuestPicker/GuestPicker"
+import CreateRoom from "./CreateRoom/CreateRoom"
+import Button from "./Shared/Button/Button"
+import ButtonSpinner from "./Shared/ButtonSpinner/ButtonSpinner"
 
-export { SearchHotel, Input }
+export { SearchHotel, Input, GuestPicker, CreateRoom, Button, ButtonSpinner }
